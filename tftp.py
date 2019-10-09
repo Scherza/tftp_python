b1bd8cc0039a5b0359dba502eb7cb328d86e7505
Scherza <coldx851@gmail.com>
2019-10-09 12:09:24 -0800

    Update tftp.py
    corrected to requirements: acquiring listening port from thread identity.

--- tftp.py
+++ tftp.py
@@ -3,6 +3,7 @@
 import argparse
 import tftp_send
 import tftp_receive
+import threading
 
 # Building variables, parsing arguments.
 from support import perror
@@ -11,7 +12,8 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-a', type=str, required=True, 
 	help='IPv4 address for the server.')
 parser.add_argument('-sp', type=int, required=True)
-parser.add_argument('-cp', type=int, required=True)
+## parser.add_argument('-cp', type=int, required=True)
+## going to grab the thread ID for this.
 parser.add_argument('-m', type=str, required=True) 
 parser.add_argument('-f', type=str, required=True,
 	help='Designates file to write to or read from.')
@@ -20,7 +22,7 @@ args = parser.parse_args()
 
 server_ip = args.a
 server_port = args.sp
-client_port = args.cp
+client_port = threading.get_ident() ## formerly args.cp
 filename = args.f
 mode = args.m
 
